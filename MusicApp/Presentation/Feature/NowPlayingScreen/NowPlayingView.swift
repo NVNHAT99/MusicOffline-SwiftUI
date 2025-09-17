2fba7dfa921946a05d5c4f052a88a0e4c9c7ab2e
Nhat Nguyen <>
2025-09-17 22:18:23 +0700

    update app

--- MusicApp/Presentation/Feature/NowPlayingScreen/NowPlayingView.swift
+++ MusicApp/Presentation/Feature/NowPlayingScreen/NowPlayingView.swift
@@ -139,179 +139,181 @@ struct NowPlayingView: View {
     // MARK: - Full Player View
     private func fullPlayer() -> some View {
         GeometryReader { geometry in
-            VStack {
-                // Custom Navigation Bar
-                CustomNavigationBar(type: .custom(title: "",
-                                                  left: .init(icon: "chevron.down",
-                                                              action: {
-                    withAnimation {
-                        isExpanded = false
-                    }
-                }),
-                                                  right: .init(icon: "gearshape.fill",
-                                                               action: {
-                    router.route(to: .showMenuBottomSheet({
-                        Task { @MainActor in
-                            viewModel.send(.cancelSleepTime)
-                            router.dismiss()
+            ScrollView {
+                VStack {
+                    // Custom Navigation Bar
+                    CustomNavigationBar(type: .custom(title: "",
+                                                      left: .init(icon: "chevron.down",
+                                                                  action: {
+                        withAnimation {
+                            isExpanded = false
                         }
-                    }, {
-                        Task { @MainActor in
-                            router.dismiss()
-                            try? await Task.sleep(nanoseconds: 50_000_000) // 0.05s
-                            router.route(to: .setSleepTime({ hours, minute, second in
-                                Task {
-                                    await viewModel.send(.setSleepTime(hours, minute, second))
-                                }
-                            }))
-                        }
-                    }))
-                })))
-                
-                Spacer()
-                    .frame(height: 16)
-                
-                // Album artwork
-                if let uiImage = uiImage {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: geometry.size.width * 4 / 5, height: geometry.size.width * 4 / 5)
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 10)
-                } else {
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(Color.gray.opacity(0.3))
-                        .frame(width: geometry.size.width * 4 / 5, height: geometry.size.width * 4 / 5)
-                        .shimmer()
-                }
-                Spacer()
-                    .frame(height: 44)
-                
-                // Song info and controls
-                VStack(alignment: .leading) {
-                    VStack(spacing: 4) {
-                        Text(viewModel.songTitle)
-                            .font(.system(size: 18, weight: .semibold))
-                            .foregroundStyle(.white)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Text(viewModel.artistName)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundStyle(.white)
-                            .font(.system(size: 14))
-                    }
+                    }),
+                                                      right: .init(icon: "gearshape.fill",
+                                                                   action: {
+                        router.route(to: .showMenuBottomSheet({
+                            Task { @MainActor in
+                                viewModel.send(.cancelSleepTime)
+                                router.dismiss()
+                            }
+                        }, {
+                            Task { @MainActor in
+                                router.dismiss()
+                                try? await Task.sleep(nanoseconds: 50_000_000) // 0.05s
+                                router.route(to: .setSleepTime({ hours, minute, second in
+                                    Task {
+                                        await viewModel.send(.setSleepTime(hours, minute, second))
+                                    }
+                                }))
+                            }
+                        }))
+                    })))
                     
                     Spacer()
-                        .frame(height: 32)
+                        .frame(height: 16)
                     
-                    // Progress slider
-                    VStack {
-                        CustomSliderView(value: $viewModel.state.currentTime,
-                                         isDragSliderView: $viewModel.state.isDraging,
-                                         minValue: 0,
-                                         maxValue: viewModel.state.duration,
-                                         trackColor: Color.init(hexString: "#808080"),
-                                         progressColor: Color.white) { value in
-                            viewModel.send(.seekTo(value))
+                    // Album artwork
+                    if let uiImage = uiImage {
+                        Image(uiImage: uiImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: geometry.size.height * 3 / 5, height: geometry.size.height * 3 / 5)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                            .shadow(color: .black.opacity(0.3), radius: 20, x: 0, y: 10)
+                    } else {
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(Color.gray.opacity(0.3))
+                            .frame(width: geometry.size.height * 3 / 5, height: geometry.size.height * 3 / 5)
+                            .shimmer()
+                    }
+                    Spacer()
+                        .frame(height: 44)
+                    
+                    // Song info and controls
+                    VStack(alignment: .leading) {
+                        VStack(spacing: 4) {
+                            Text(viewModel.songTitle)
+                                .font(.system(size: 18, weight: .semibold))
+                                .foregroundStyle(.white)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text(viewModel.artistName)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .foregroundStyle(.white)
+                                .font(.system(size: 14))
                         }
-                                         .frame(height: 12)
                         
+                        Spacer()
+                            .frame(height: 32)
+                        
+                        // Progress slider
+                        VStack {
+                            CustomSliderView(value: $viewModel.state.currentTime,
+                                             isDragSliderView: $viewModel.state.isDraging,
+                                             minValue: 0,
+                                             maxValue: viewModel.state.duration,
+                                             trackColor: Color.init(hexString: "#808080"),
+                                             progressColor: Color.white) { value in
+                                viewModel.send(.seekTo(value))
+                            }
+                                             .frame(height: 12)
+                            
+                            HStack {
+                                Text(viewModel.currentTimeStr)
+                                    .foregroundStyle(.white)
+                                Spacer()
+                                Text(viewModel.state.duration.toTimeString())
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                        
+                        Spacer()
+                            .frame(height: 24)
+                        
+                        // Control buttons
                         HStack {
-                            Text(viewModel.currentTimeStr)
-                                .foregroundStyle(.white)
+                            Button {
+                                viewModel.send(.changeShuffMode)
+                            } label: {
+                                Image(systemName: "shuffle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .aspectRatio(1.0, contentMode: .fit)
+                                    .foregroundStyle(viewModel.shuffleButtonColor)
+                                    .frame(width: 28)
+                            }
+                            
                             Spacer()
-                            Text(viewModel.state.duration.toTimeString())
-                                .foregroundStyle(.white)
-                        }
-                    }
-                    
-                    Spacer()
-                        .frame(height: 24)
-                    
-                    // Control buttons
-                    HStack {
-                        Button {
-                            viewModel.send(.changeShuffMode)
-                        } label: {
-                            Image(systemName: "shuffle")
-                                .resizable()
-                                .scaledToFit()
-                                .aspectRatio(1.0, contentMode: .fit)
-                                .foregroundStyle(viewModel.shuffleButtonColor)
-                                .frame(width: 28)
-                        }
-                        
-                        Spacer()
-                        
-                        Button {
-                            viewModel.send(.goPrevious)
-                        } label: {
-                            Image(systemName: "arrowtriangle.backward.fill")
-                                .resizable()
-                                .scaledToFit()
-                                .aspectRatio(1.0, contentMode: .fit)
-                                .foregroundStyle(.white)
-                                .frame(width: 28)
-                        }
-                        
-                        Spacer()
-                        
-                        Button {
-                            viewModel.send(.togglePlay)
-                        } label: {
-                            Image(systemName: viewModel.playButtonIcon)
-                                .resizable()
-                                .scaledToFit()
-                                .aspectRatio(1.0, contentMode: .fit)
-                                .foregroundStyle(.white)
-                                .frame(width: 54)
-                        }
-                        
-                        Spacer()
-                        
-                        Button {
-                            viewModel.send(.goNext)
-                        } label: {
-                            Image(systemName: "arrowtriangle.right.fill")
-                                .resizable()
-                                .scaledToFit()
-                                .aspectRatio(1.0, contentMode: .fit)
-                                .foregroundStyle(.white)
-                                .frame(width: 28)
-                        }
-                        
-                        Spacer()
-                        
-                        Button {
-                            viewModel.send(.changeRepeatMode)
-                        } label: {
-                            Image(systemName: viewModel.repeatButtonIcon)
-                                .resizable()
-                                .scaledToFit()
-                                .aspectRatio(1.0, contentMode: .fit)
-                                .foregroundStyle(viewModel.repeatButtonColor)
-                                .frame(width: 28)
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding(.horizontal, geometry.size.width * 1 / 10)
-                
-                Spacer()
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .padding(.top, Helper.shared.safeAreaInsets?.top)
-            .background(Color.backgroundColor)
-            .gesture(
-                DragGesture()
-                    .onEnded { gesture in
-                        if gesture.translation.height > 100 {
-                            withAnimation {
-                                isExpanded = false
+                            
+                            Button {
+                                viewModel.send(.goPrevious)
+                            } label: {
+                                Image(systemName: "arrowtriangle.backward.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .aspectRatio(1.0, contentMode: .fit)
+                                    .foregroundStyle(.white)
+                                    .frame(width: 28)
+                            }
+                            
+                            Spacer()
+                            
+                            Button {
+                                viewModel.send(.togglePlay)
+                            } label: {
+                                Image(systemName: viewModel.playButtonIcon)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .aspectRatio(1.0, contentMode: .fit)
+                                    .foregroundStyle(.white)
+                                    .frame(width: 54)
+                            }
+                            
+                            Spacer()
+                            
+                            Button {
+                                viewModel.send(.goNext)
+                            } label: {
+                                Image(systemName: "arrowtriangle.right.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .aspectRatio(1.0, contentMode: .fit)
+                                    .foregroundStyle(.white)
+                                    .frame(width: 28)
+                            }
+                            
+                            Spacer()
+                            
+                            Button {
+                                viewModel.send(.changeRepeatMode)
+                            } label: {
+                                Image(systemName: viewModel.repeatButtonIcon)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .aspectRatio(1.0, contentMode: .fit)
+                                    .foregroundStyle(viewModel.repeatButtonColor)
+                                    .frame(width: 28)
                             }
                         }
                     }
-            )
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, geometry.size.width * 1 / 10)
+                    
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(.top, Helper.shared.safeAreaInsets?.top)
+                .background(Color.backgroundColor)
+                .gesture(
+                    DragGesture()
+                        .onEnded { gesture in
+                            if gesture.translation.height > 100 {
+                                withAnimation {
+                                    isExpanded = false
+                                }
+                            }
+                        }
+                )
+            }.scrollIndicators(.hidden)
         }
         
     }
